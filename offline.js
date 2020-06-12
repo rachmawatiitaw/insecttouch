7809a7ee8aee9ca23e1eddd701ab07de09ec73ca
rachmawatiitaw <rachma.pti17@gmail.com>
2020-06-12 15:00:28 +0700

    insecttouch
    Membunuh nyamuk

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1591495563,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg_depan-sheet0.png",
+		"images/judul-sheet0.png",
+		"images/play-sheet0.png",
+		"images/bg_main-sheet0.png",
+		"images/bee-sheet0.png",
+		"images/nyamuk-sheet0.png",
+		"images/live-sheet0.png",
+		"images/bt_again-sheet0.png",
+		"images/bg_gameover-sheet0.png",
+		"images/particles.png",
+		"images/particles2.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
